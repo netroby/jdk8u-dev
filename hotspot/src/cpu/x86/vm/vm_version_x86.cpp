6ae32719a2b6d99d7ca26a372383082b8ab69484
ZhiFeng Hu <hufeng1987@gmail.com>
2016-03-10 12:08:47 +0800

    sync code 2016-03-10
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- hotspot/src/cpu/x86/vm/vm_version_x86.cpp
+++ hotspot/src/cpu/x86/vm/vm_version_x86.cpp
@@ -704,10 +704,16 @@ void VM_Version::get_processor_features() {
     UseMultiplyToLenIntrinsic = true;
   }
   if (FLAG_IS_DEFAULT(UseSquareToLenIntrinsic)) {
-    UseSquareToLenIntrinsic = true;
+    UseSquareToLenIntrinsic = false;
   }
   if (FLAG_IS_DEFAULT(UseMulAddIntrinsic)) {
-    UseMulAddIntrinsic = true;
+    UseMulAddIntrinsic = false;
+  }
+  if (FLAG_IS_DEFAULT(UseMontgomeryMultiplyIntrinsic)) {
+    UseMontgomeryMultiplyIntrinsic = false;
+  }
+  if (FLAG_IS_DEFAULT(UseMontgomerySquareIntrinsic)) {
+    UseMontgomerySquareIntrinsic = false;
   }
 #else
   if (UseMultiplyToLenIntrinsic) {
@@ -727,6 +733,18 @@ void VM_Version::get_processor_features() {
       warning("mulAdd intrinsic is not available in 32-bit VM");
     }
     FLAG_SET_DEFAULT(UseMulAddIntrinsic, false);
+  }
+  if (UseMontgomeryMultiplyIntrinsic) {
+    if (!FLAG_IS_DEFAULT(UseMontgomeryMultiplyIntrinsic)) {
+      warning("montgomeryMultiply intrinsic is not available in 32-bit VM");
+    }
+    FLAG_SET_DEFAULT(UseMontgomeryMultiplyIntrinsic, false);
+  }
+  if (UseMontgomerySquareIntrinsic) {
+    if (!FLAG_IS_DEFAULT(UseMontgomerySquareIntrinsic)) {
+      warning("montgomerySquare intrinsic is not available in 32-bit VM");
+    }
+    FLAG_SET_DEFAULT(UseMontgomerySquareIntrinsic, false);
   }
 #endif
 #endif // COMPILER2
